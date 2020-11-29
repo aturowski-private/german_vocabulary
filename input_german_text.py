ad3bff29852dbe1da71421b918a46bfca87aa23f
Adam Turowski <adam.turowski@broadcom.com>
2020-11-29 11:24:22 +0000

    German characters work with Python3

--- input_german_text.py
+++ input_german_text.py
@@ -1,5 +1,6 @@
 import curses
 import locale
+from german_dictionary import Au, au, Ea, ea, Ou, ou, Uu, uu, Ss
 
 class InputGermanText:
     def __init__(self, winPos = (1, 0), expectedText = ""):
@@ -21,9 +22,42 @@ class InputGermanText:
         self.printPrompt()
         while True:
             k = self.win.getch()
+            # now translate the key press into correct action
             if (k == curses.KEY_ENTER) or (k == 10) or (k == 13):
+                # user pressed Enter
                 break
-            self.win.addstr(1, self.pos, chr(k))
+            elif (chr(k) == '1'):
+                # german capital A umlaut
+                char = Au
+            elif (chr(k) == '2'):
+                # german lower a umlaut
+                char = au
+            elif (chr(k) == '3'):
+                # german capital E acute
+                char = Ea
+            elif (chr(k) == '4'):
+                # german lower E acute
+                char = ea
+            elif (chr(k) == '5'):
+                # german capital O umlaut
+                char = Ou
+            elif (chr(k) == '6'):
+                # german lower o umlaut
+                char = ou
+            elif (chr(k) == '7'):
+                # german capital U umlaut
+                char = Uu
+            elif (chr(k) == '8'):
+                # german lower u umlaut
+                char = uu
+            elif (chr(k) == '9'):
+                # german s ligature
+                char = Ss
+            elif (chr(k).isalpha()):
+                # normal latin letters
+                char = chr(k)
+
+            self.win.addstr(1, self.pos, char.encode('utf-8'))
             self.pos = self.pos + 1
-            self.input = self.input + chr(k)
+            self.input = self.input + char
         return self.input
--- try_curses.py
+++ try_curses.py
@@ -1,29 +1,32 @@
 import curses
 import locale
 from german_dictionary import vocabulary, ENG_IDX, GER_IDX
-
+from input_german_text import InputGermanText
 # positions to where display things in ncurses window (col, row)
 ENGLISH_WORD_POS = (0, 0)
 GERMAN_GENDER_POS = (0, 1)
 GERMAN_WORD_POS = (4, 1)
 
-def display_word_to_translate(word, stdscr):
+def display_word_to_translate(word, stdscr, code):
     print(word)
     # display English word
     stdscr.addstr(  ENGLISH_WORD_POS[1],
                     ENGLISH_WORD_POS[0],
-                    word[ENG_IDX])
+                    word[ENG_IDX].encode(code))
     # display German word
     stdscr.addstr(  GERMAN_GENDER_POS[1],
                     GERMAN_GENDER_POS[0],
-                    word[GER_IDX][0])
+                    word[GER_IDX][0].encode(code))
     stdscr.addstr(  GERMAN_WORD_POS[1],
                     GERMAN_WORD_POS[0],
-                    word[GER_IDX][1])
+                    word[GER_IDX][1].encode(code))
     stdscr.refresh()
 
 
 def main(stdscr):
+    locale.setlocale(locale.LC_ALL, '')
+    code = locale.getpreferredencoding()
+
     # clear screen
     stdscr.clear()
     curses.noecho()
@@ -31,14 +34,15 @@ def main(stdscr):
     stdscr.keypad(True)
     
     # select the word to display
-    word = vocabulary[0]['words'][1]
-    print(vocabulary)
-    print(word)
+    word = vocabulary[0]['words'][3]
+    # print(vocabulary)
+    # print(word)
 
     # display something
-    display_word_to_translate(word, stdscr)
-
-    stdscr.getch()
+    # display_word_to_translate(word, stdscr, code)
+    # stdscr.getch()
+    inputWindow = InputGermanText()
+    inputWindow.getInput()
 
     # finish the application
     stdscr.keypad(False)
